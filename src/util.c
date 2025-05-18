a2393cfb65d97671196f1cc02d662bd50e734e6d
DylanCoon99 <dylcoon99@gmail.com>
2025-05-17 20:56:46 -0400

    implemented first fit

--- src/util.c
+++ src/util.c
@@ -14,11 +14,38 @@ size_t get_data_size(struct Block* block){
 
 struct Block* first_fit(size_t size) {
 
+	struct Block* current = memory_allocator.free_list;
+
+    while (current != NULL) {
+        if (current->free && current->size >= size) {
+        	split_block(current, size);
+            return current;  // Found a suitable block
+        }
+        current = current->next;
+    }
+
+    return NULL;  
+
+};
+
+
+struct Block* best_fit(size_t size) {
+
 	struct Block* free_list = memory_allocator.free_list; 
+
+	printf("This is the size for debugging purposes: %zu\n", size);
 
 	return free_list;
 
 };
 
-struct Block* best_fit(size_t size);
-struct Block* worst_fit(size_t size);
+
+struct Block* worst_fit(size_t size) {
+
+	struct Block* free_list = memory_allocator.free_list; 
+
+	printf("This is the size for debugging purposes: %zu\n", size);
+
+	return free_list;
+
+};
